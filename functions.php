50b52ed6de610e9dbbe7ca9a303e2fb73e89a9a7
sara-p- <sarapitt88@gmail.com>
2025-08-06 15:59:37 -0400

    initial commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * BookList functions and definitions.
+ *
+ *
+ * @package WordPress
+ * @subpackage BookList
+ * @since BookList 1.0
+ */
+
+/* 
+Custom REST API
+*/
+require get_theme_file_path( '/inc/book-route.php' );
---
+++ inc/book-route.php
@@ -0,0 +1,135 @@
+<?php
+// Callback that supplies a custom data array for the custom API endpoint
+function booklist_api_endpoint_callback( $request ) {
+  $mainQuery = new WP_Query(array(
+    'post_type' => array('book'),
+    'posts_per_page' => -1
+  ));
+
+  $bookResults = array();
+
+  while($mainQuery->have_posts()) {
+    $mainQuery->the_post();
+
+    // Author array 
+    $authorTerms = get_the_terms( get_the_ID(), 'author' );
+    $authorArray = array();
+    foreach($authorTerms as $author) {
+      array_push($authorArray, array(
+        'authorId' => $author->term_id,
+        'name' => $author->name,
+        'slug' => $author->slug
+      ));
+    }
+    
+    // Series Array
+    $seriesTerms = get_the_terms( get_the_ID(), 'series' );
+    $seriesArray = array();
+    foreach($seriesTerms as $series) {
+      array_push($seriesArray, array(
+        'seriesId' => $series->term_id,
+        'name' => $series->name,
+        'slug' => $series->slug
+      ));
+    }
+
+    // Series Array
+    $seriesTerms = get_the_terms( get_the_ID(), 'series' );
+    $seriesArray = array();
+    foreach($seriesTerms as $series) {
+      array_push($seriesArray, array(
+        'seriesId' => $series->term_id,
+        'name' => $series->name,
+        'slug' => $series->slug
+      ));
+    }
+    
+    // Genre Array
+    $genreTerms = get_the_terms( get_the_ID(), 'genre' );
+    $genreArray = array();
+    foreach($genreTerms as $genre) {
+      array_push($genreArray, array(
+        'genreId' => $genre->term_id,
+        'name' => $genre->name,
+        'slug' => $genre->slug
+      ));
+    }
+    
+    // Trope Array
+    $tropeTerms = get_the_terms( get_the_ID(), 'trope' );
+    $tropeArray = array();
+    foreach($tropeTerms as $trope) {
+      array_push($tropeArray, array(
+        'tropeId' => $trope->term_id,
+        'name' => $trope->name,
+        'slug' => $trope->slug
+      ));
+    }
+    
+    // Creature Array
+    $creatureTerms = get_the_terms( get_the_ID(), 'creature' );
+    $creatureArray = array();
+    foreach($creatureTerms as $creature) {
+      array_push($creatureArray, array(
+        'creatureId' => $creature->term_id,
+        'name' => $creature->name,
+        'slug' => $creature->slug
+      ));
+    }
+
+    // BookTag Array
+    $booktagTerms = get_the_terms( get_the_ID(), 'booktag' );
+    $booktagArray = array();
+    foreach($booktagTerms as $booktag) {
+      array_push($booktagArray, array(
+        'booktagId' => $booktag->term_id,
+        'name' => $booktag->name,
+        'slug' => $booktag->slug
+      ));
+    }
+
+
+
+    // Push everything into the bookResults array
+    array_push($bookResults, array(
+      'bookId' => get_the_ID(),
+      'title' => get_the_title(),
+      'author' => $authorArray,
+      'series' => $seriesArray,
+      'image' => get_field('image'),
+      'genres' => $genreArray,
+      'tropes' => $tropeArray,
+      'creatures' => $creatureArray,
+      'booktags' => $booktagArray,
+      'bookNumber' => get_field('book_number'),
+      'publishDate' => get_field('publish_date'),
+      'length' => get_field('length'),
+      'rating' => get_field('rating'),
+      'spice' => get_field('spice'),
+      'finished' => get_field('finished'),
+      'amountCompleted' => get_field('amount_completed'),
+      'display' => get_field('display'),
+      'description' => get_field('description'),
+      'notes' => get_field('notes'),
+      'smell' => get_field('smell'),
+      'startDate' => get_field('start_date'),
+      'finishDate' => get_field('finish_date'),
+      'goodreadsLink' => get_field('goodreads_link'),
+      'amazonLink' => get_field('amazon_link'),
+    ));
+  }
+	
+
+
+    return rest_ensure_response( $bookResults );
+}
+
+// Adds theme support for custom books API endpoint
+function register_booklist_api_endpoint() {
+    register_rest_route( 'booklist/v1', '/books/', array(
+        'methods'             => WP_REST_SERVER::READABLE,
+        'callback'            => 'booklist_api_endpoint_callback',
+        'permission_callback' => '__return_true', // For public access, or define a custom permission check
+    ));
+}
+add_action( 'rest_api_init', 'register_booklist_api_endpoint' );
